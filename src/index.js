410fddf026af232c41e3bcc556d8da62dbbf40ce
Svetlana Tur <svetaltur@gmail.com>
2018-02-25 12:06:56 +0200

    solved.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,10 @@ for (var k=0; k<str.length; k++){
  for (var i=0; i<bracketsConfig.length; i++){
    var c=bracketsConfig[i];
    
+
        if (str.substr(k,1)==c[0]){
          s.push(c[0]);
-         
        }else {
-         
                if (str.substr(k,1)==c[1]){
                    if (s.length==0){
                      return false;
@@ -20,7 +19,17 @@ for (var k=0; k<str.length; k++){
                       }
                     }          
                 }
-              }         
+                
+              } 
+              
+                if (c[0]==c[1]){
+                  for (var t=(s.length)-1; t>=0; t--){
+                    if (s[t]==s[t-1] && s[t]==c[0]){
+                     s.splice(t-1,2);
+                      
+                    }
+                  }
+                }     
        }  
    }
    
